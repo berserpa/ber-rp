0496349907b898eb63d26084c98143d8313f4cdf
berserpa <bernarserpaalvares@gmail.com>
2024-10-31 16:31:46 -0300

    Update buscador.js

--- buscador.js
+++ buscador.js
@@ -1,6 +1,6 @@
 
 
-fetch('https://fakestoreapi.com/products?limit=10')
+fetch('https://fakestoreapi.com/products?limit=20')
     .then(res => res.json())
     .then((json) => {
         console.log(json);
